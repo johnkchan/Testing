5131e8fe0e0ac28d6c4dc0c520b3b163bf7651d7
Jun Hao Huang <junhao.huang524@gmail.com>
2018-02-22 22:28:44 -0500

    Update test.js

--- assets/javascript/test.js
+++ assets/javascript/test.js
@@ -2,5 +2,11 @@ $(document).ready(function() {
   
   $("#hello").click(function(){
     console.log("hello world");
-  })
+  });
+  $( "#target" ).click(function() {
+  alert( "Handler for .click() called." );
+  });
+  $( "#other" ).click(function() {
+  $( "#target" ).click();
+  });
 })
