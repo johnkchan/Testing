d9f8ca052f1bf8efa9a2feae3a03168fbed481fc
Jonathan Chan <johnkchan@users.noreply.github.com>
2018-03-09 00:38:31 -0500

    Update and rename test.js to index.js

---
+++ assets/javascript/index.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+
+  $("#restartBtn").on("click", function(){
+    $("td").each(function(){
+      $("td").text("");
+    });
+    $("#message").text("Welcome to Tic Tac Toe");
+  });
+
+  var turn = 0;
+  $("td").on("click", function(){
+    if (turn % 2 == 0){
+      $(this).text("X");
+    } else {
+      $(this).text("O");
+    }
+    turn++;
+    $(this).prop("disabled", true);
+    checkWinner($(this));
+  });
+
+  function checkWinner(id){
+    // Horizontal Victory
+    if ($("#cell1").text() != "" && $("#cell1").text() == $("#cell2").text() && $("#cell1").text() == $("#cell3").text()){
+      $("#message").text($("#cell1").text() + " has won!");
+    } else if ($("#cell4").text() != "" && $("#cell4").text() == $("#cell5").text() && $("#cell4").text() == $("#cell6").text()){
+      $("#message").text($("#cell4").text() + " has won!");
+    } else if ($("#cell7").text() != "" && $("#cell7").text() == $("#cell8").text() && $("#cell7").text() == $("#cell9").text()){
+      $("#message").text($("#cell7").text() + " has won!");
+    }
+    // Vertical Victory
+    if ($("#cell1").text() != "" && $("#cell1").text() == $("#cell4").text() && $("#cell1").text() == $("#cell7").text()){
+      $("#message").text($("#cell1").text() + " has won!");
+    } else if ($("#cell2").text() != "" && $("#cell2").text() == $("#cell5").text() && $("#cell2").text() == $("#cell8").text()){
+      $("#message").text($("#cell2").text() + " has won!");
+    } else if ($("#cell3").text() != "" && $("#cell3").text() == $("#cell6").text() && $("#cell3").text() == $("#cell9").text()){
+      $("#message").text($("#cell3").text() + " has won!");
+    }
+    // Diagonal Victory
+    if ($("#cell1").text() != "" && $("#cell1").text() == $("#cell5").text() && $("#cell1").text() == $("#cell9").text()){
+      $("#message").text($("#cell1").text() + " has won!");
+    } else if ($("#cell3").text() != "" && $("#cell3").text() == $("#cell5").text() && $("#cell3").text() == $("#cell7").text()){
+      $("#message").text($("#cell3").text() + " has won!");
+    }
+  }
+
+})
